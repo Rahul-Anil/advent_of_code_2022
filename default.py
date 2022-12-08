906e7fe6f15ab07924b4d431802975278fea744a
Rahul Anil <rahulanil101@gmail.cpm>
2022-12-08 10:05:32 +0530

    new default

--- default.py
+++ default.py
@@ -1,34 +1,9 @@
-import math
-import numpy as np
-from collections import defaultdict, Counter
-
-
-def part1(input: str) -> int:
-    print(f"input: {input}")
-    return 0
-
-
-def part2(input: str) -> int:
-    return 0
-
-
-def test():
-    print(f"TEST")
-    with open("./input/sample_inputs/day_3_sample.txt") as f:
-        input = f.read()
-
-    print(f"P1: {part1(input)}")
-    assert part1(input) == 0
-    print(f"P2: {part2(input)}")
-    assert part2(input) == 0
-    print("\n")
+def solve(input: str):
+    pass
 
 
 if __name__ == "__main__":
-    with open("./input/day3.txt") as f:
-        input = f.read()
-
-    test()
-    print("SOLUTION")
-    # print(f"P1: {part1(input)}")
-    # print(f"P2: {part2(input)}")
+    input = open("./input/sample_inputs/day_7_sample.txt").read().strip()
+    inputMain = open("./input/day7.txt").read().strip()
+    solve(input)
+    # solve(inputMain)
