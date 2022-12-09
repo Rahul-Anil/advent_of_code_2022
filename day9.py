84ca1b0d219e7fea122f3b2355b49617c009fb87
Rahul Anil <rahulanil101@gmail.cpm>
2022-12-09 13:36:50 +0530

    day 9 done personal method

--- day9.py
+++ day9.py
@@ -1,9 +1,14 @@
-def solve1(input: str):
+def isAdj(x1, y1, x2, y2):
+    return abs(x1 - x2) <= 1 and abs(y1 - y2) <= 1
+
+
+def solve1(input):
+    hx, hy = 0, 0
+    tx, ty = 0, 0
     visited = set()
-    hx, hy, tx, ty = 0, 0, 0, 0
-    for line in input.strip().split("\n"):
-        direction, step = line.strip().split()
-        for _ in range(int(step)):
+    for line in input.strip().splitlines():
+        direction, steps = line.strip().split()
+        for _ in range(int(steps)):
             if direction == "R":
                 hy += 1
             elif direction == "L":
@@ -13,81 +18,55 @@ def solve1(input: str):
             elif direction == "D":
                 hx -= 1
 
-            if (max(abs(hx - tx), abs(hy - ty))) > 1:
-                if hx - tx == 0:
-                    tx += 0
-                elif hx - tx <= -1:
-                    tx += -1
-                else:
-                    tx += 1
-                if hy - ty == 0:
-                    ty += 0
-                elif hy - ty <= -1:
-                    ty += -1
-                else:
-                    ty += 1
+            if not isAdj(hx, hy, tx, ty):
+                sx = 0 if hx == tx else (hx - tx) / abs(hx - tx)
+                sy = 0 if hy == ty else (hy - ty) / abs(hy - ty)
+
+                tx += sx
+                ty += sy
+                # print(f"tx: {tx}, ty: {ty}")
 
             visited.add((tx, ty))
-
     print(f"count: {len(visited)}")
-    pass
 
 
 def solve2(input: str):
-    knotMoves = {
-        (0, 2): (0, 1),
-        (0, -2): (0, -1),
-        (2, 0): (1, 0),
-        (-2, 0): (-1, 0),
-        (1, 2): (1, 1),
-        (-1, 2): (-1, 1),
-        (1, -2): (1, -1),
-        (-1, -2): (-1, -1),
-        (2, 1): (1, 1),
-        (2, -1): (1, -1),
-        (-2, 1): (-1, 1),
-        (-2, -1): (-1, -1),
-        (2, 2): (1, 1),
-        (2, -2): (1, -1),
-        (-2, 2): (-1, 1),
-        (-2, -2): (-1, -1),
-    }
     visited = set()
-    knotPos = [(0, 0) for _ in range(10)]
+    knot = [[0, 0] for _ in range(10)]
     for line in input.strip().splitlines():
-        print(line)
         direction, steps = line.strip().split()
         for _ in range(int(steps)):
-            if direction == "U":
-                knotPos[0] = (knotPos[0][0], knotPos[0][1] + 1)
-            elif direction == "D":
-                knotPos[0] = (knotPos[0][0], knotPos[0][1] - 1)
+            if direction == "R":
+                knot[0][1] += 1
             elif direction == "L":
-                knotPos[0] = (knotPos[0][0] - 1, knotPos[0][1])
-            elif direction == "R":
-                knotPos[0] = (knotPos[0][0] + 1, knotPos[0][1])
-            for i in range(1, 10):
-                if (
-                    max(
-                        abs(knotPos[i - 1][0] - knotPos[i][0]),
-                        abs(knotPos[i - 1][1] - knotPos[i][1]),
-                    )
-                    > 1
-                ):
-                    key = (
-                        knotPos[i - 1][0] - knotPos[i][0],
-                        knotPos[i - 1][1] - knotPos[i][1],
-                    )
-                    knotPos[i] = (
-                        knotPos[i][0] + knotMoves[key][0],
-                        knotPos[i][1] + knotMoves[key][1],
-                    )
-            visited.add(knotPos[9])
-    print(len(visited))
+                knot[0][1] -= 1
+            elif direction == "U":
+                knot[0][0] += 1
+            elif direction == "D":
+                knot[0][0] -= 1
+
+            for k in range(1, 10):
+                hx, hy = knot[k - 1]
+                tx, ty = knot[k]
+
+                if not isAdj(hx, hy, tx, ty):
+                    sx = 0 if hx == tx else (hx - tx) / abs(hx - tx)
+                    sy = 0 if hy == ty else (hy - ty) / abs(hy - ty)
+
+                    tx += sx
+                    ty += sy
+
+                    knot[k] = [tx, ty]
+
+            visited.add(tuple(knot[9]))
+    print(f"count: {len(visited)}")
 
 
 if __name__ == "__main__":
-    input = open("./input/sample_inputs/day_9_sample.txt").read().strip()
+    input1 = open("./input/sample_inputs/day_9_sample1.txt").read().strip()
+    input2 = open("./input/sample_inputs/day_9_sample.txt").read().strip()
     inputMain = open("./input/day9.txt").read().strip()
-    solve2(input)
+    solve1(input1)
+    solve1(inputMain)
+    solve2(input2)
     solve2(inputMain)
