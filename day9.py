a4e660be7b1de022c526829d66930bd9d85addf2
Rahul Anil Damodaran <rahulanil103@gmail.com>
2022-12-09 12:29:58 +0530

    day 9 done

--- day9.py
+++ day9.py
@@ -1,4 +1,4 @@
-def solve(input: str):
+def solve1(input: str):
     visited = set()
     hx, hy, tx, ty = 0, 0, 0, 0
     for line in input.strip().split("\n"):
@@ -33,8 +33,61 @@ def solve(input: str):
     pass
 
 
+def solve2(input: str):
+    knotMoves = {
+        (0, 2): (0, 1),
+        (0, -2): (0, -1),
+        (2, 0): (1, 0),
+        (-2, 0): (-1, 0),
+        (1, 2): (1, 1),
+        (-1, 2): (-1, 1),
+        (1, -2): (1, -1),
+        (-1, -2): (-1, -1),
+        (2, 1): (1, 1),
+        (2, -1): (1, -1),
+        (-2, 1): (-1, 1),
+        (-2, -1): (-1, -1),
+        (2, 2): (1, 1),
+        (2, -2): (1, -1),
+        (-2, 2): (-1, 1),
+        (-2, -2): (-1, -1),
+    }
+    visited = set()
+    knotPos = [(0, 0) for _ in range(10)]
+    for line in input.strip().splitlines():
+        print(line)
+        direction, steps = line.strip().split()
+        for _ in range(int(steps)):
+            if direction == "U":
+                knotPos[0] = (knotPos[0][0], knotPos[0][1] + 1)
+            elif direction == "D":
+                knotPos[0] = (knotPos[0][0], knotPos[0][1] - 1)
+            elif direction == "L":
+                knotPos[0] = (knotPos[0][0] - 1, knotPos[0][1])
+            elif direction == "R":
+                knotPos[0] = (knotPos[0][0] + 1, knotPos[0][1])
+            for i in range(1, 10):
+                if (
+                    max(
+                        abs(knotPos[i - 1][0] - knotPos[i][0]),
+                        abs(knotPos[i - 1][1] - knotPos[i][1]),
+                    )
+                    > 1
+                ):
+                    key = (
+                        knotPos[i - 1][0] - knotPos[i][0],
+                        knotPos[i - 1][1] - knotPos[i][1],
+                    )
+                    knotPos[i] = (
+                        knotPos[i][0] + knotMoves[key][0],
+                        knotPos[i][1] + knotMoves[key][1],
+                    )
+            visited.add(knotPos[9])
+    print(len(visited))
+
+
 if __name__ == "__main__":
     input = open("./input/sample_inputs/day_9_sample.txt").read().strip()
     inputMain = open("./input/day9.txt").read().strip()
-    solve(input)
-    solve(inputMain)
+    solve2(input)
+    solve2(inputMain)
