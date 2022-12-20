c978ae85163230dd86e2d6fc7757950b5dc951da
Rahul Anil Damodaran <rahulanil101@gmail.com>
2022-12-20 13:03:13 +0530

    replacement of old file structure and new file struc in its place

--- day16/day16p1.py
+++ day16/day16p1.py
@@ -1,10 +1,37 @@
-f = {}
-p = {}
+from collections import deque
+
+valves = {}
+tunnels = {}
 
 for line in open(0).read().strip().splitlines():
-    a, b = line.split(";")
-    f[a[6:8]] = int(a.split("=")[1])
-    p[a[6:8]] = "".join(b.strip().split(" ")[4:]).split(",")
-    # print("".join(b.strip().split(" ")[4:]).split(","))
+    valve = line.split()[1]
+    flow = int(line.split(";")[0].split("=")[1])
+    targets = line.split("to ")[1].split(" ", 1)[1].split(", ")
+    valves[valve] = flow
+    tunnels[valve] = targets
 
-t = 0
+dists = {}
+
+for valve in valves:
+    if valve != "AA" and not valves[valve]:
+        continue
+
+    dists[valve] = {}
+    visited = {valve}
+
+    q = deque([(0, valve)])
+
+    while q:
+        dis, pos = q.popleft()
+        for neigh in tunnels[pos]:
+            if neigh in visited:
+                continue
+            visited.add(neigh)
+
+            if valves[neigh]:
+                dists[valve][neigh] = dis + 1
+
+            q.append((dis + 1, neigh))
+
+
+print(dists)
--- default.py
+++
@@ -1,9 +0,0 @@
-def solve(input: str):
-    pass
-
-
-if __name__ == "__main__":
-    input = open("./input/sample_inputs/day_7_sample.txt").read().strip()
-    inputMain = open("./input/day7.txt").read().strip()
-    solve(input)
-    # solve(inputMain)
